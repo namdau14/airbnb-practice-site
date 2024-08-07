bd6306d31a0f35a990c27e6ae46da17d22cd4f04
namdau14 <namdau14@gmail.com>
2024-08-06 22:12:47 -0700

    fix bug

--- server/index.ts
+++ server/index.ts
@@ -88,8 +88,8 @@ const photosMiddleware = multer({dest: 'uploads/'})
 app.post('/upload', photosMiddleware.array('photos', 100), (req: any, res: any) => {
     const uploadedFiles = []
     for (let i = 0; i < req.files.length; i++) {
-        const {path, originalName} = req.files[i];
-        const parts = originalName.split('.');
+        const {path, originalname} = req.files[i];
+        const parts = originalname.split('.');
         const ext = parts[parts.length - 1]
         const newPath = path + '.' + ext;
         fs.renameSync(path, newPath)
